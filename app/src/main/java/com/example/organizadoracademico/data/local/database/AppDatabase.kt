d230c218de52b550752deac585412973800d1c88
251244@ids.upchiapas.edu.mx <251244@ids.upchiapas.edu.mx>
2026-03-02 22:36:45 -0600

    version 3 dao

--- app/src/main/java/com/example/organizadoracademico/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/example/organizadoracademico/data/local/database/AppDatabase.kt
@@ -20,7 +20,7 @@ import kotlinx.coroutines.launch
         HorarioEntity::class,
         ImagenEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
